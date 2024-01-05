8b4f3d59ea402efbffb37243203523db4737dc84
Felipe Barreto <felipebarreto1050@gmail.com>
2024-01-05 10:40:16 -0300

    Feat: looger winston

--- src/functions/login.ts
+++ src/functions/login.ts
@@ -7,6 +7,7 @@ import { LoginResponse } from "../types/LoginResponse";
 import { emailRegex, passwordRegex } from "../contents/Regexes";
 import { CognitoServices } from "../services/CognitoServices";
 import { validateEvns } from "../utils/validadeEnvs";
+import { logger } from "../utils/loggerUtils";
 export const login: Handler = async (
   event: APIGatewayEvent,
 ): Promise<DefaultJsonMessage> => {
@@ -16,6 +17,7 @@ export const login: Handler = async (
       "USER_POOL_CLIENT_ID",
     ]);
     if (error) {
+      logger.error("Login.handler - ", error);
       return formatDefaultResponse(500, error);
     }
 
@@ -33,12 +35,16 @@ export const login: Handler = async (
     if (!password || !password.match(passwordRegex)) {
       return formatDefaultResponse(400, "Senha inválido");
     }
+    logger.info("Login.handler - start", login);
     const response = await new CognitoServices(
       USER_POOL_ID,
       USER_POOL_CLIENT_ID,
     ).login(login, password);
+    logger.debug("Login.handler - cognito response:", response);
+    logger.info("Login.handler - finish", login);
     return formatDefaultResponse(200, undefined, response);
   } catch (err) {
+    logger.error("Login.handler - Error on login user:", err);
     return formatDefaultResponse(500, "erro ao autenticar usuário");
   }
 };
